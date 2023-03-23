1c6aed4c31932ef1b1e77c2e05765b8c2f562273
Martin Quintana <112594038+martinrquintan@users.noreply.github.com>
2023-03-22 22:25:34 -0300

    first commit

---
+++ src/app/videojuego/videojuego.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit, DoCheck, OnDestroy } from '@angular/core';
+
+@Component({
+	selector: 'videojuego',
+	templateUrl: './videojuego.component.html'
+})
+export class VideojuegoComponent implements OnInit, DoCheck, OnDestroy{
+	public titulo: string;
+	public listado: string;
+
+	constructor(){
+		this.titulo = "Componente de Videojuegos";
+		this.listado = "Listado de los juegos más populares";
+
+		// console.log("Se ha cargado el componente: videojuego.component.ts");
+	}
+
+	ngOnInit(){
+		// console.log("OnInit ejecutado");
+	}
+
+	ngDoCheck(){
+		// console.log("DoCheck ejecutado");
+	}
+
+	ngOnDestroy(){
+		// console.log("OnDestroy ejecutado");
+	}
+
+	cambiarTitulo(){
+		this.titulo = "Nuevo titulo del componente";
+	}
+
+}
